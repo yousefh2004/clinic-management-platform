7b94f0ddc689573bd70addaecdba04a2b2194922
Yousef Yaser Hajeer <yousefhajeer384@gmail.com>
2026-08-13 11:57:33 +0300

    add Department entity

---
+++ backend/src/main/java/org/cmp/backend/entity/Department.java
@@ -0,0 +1,29 @@
+package org.cmp.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "departments")
+public class Department extends AuditableEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+}
