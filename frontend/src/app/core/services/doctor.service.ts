9d12accc8f31c059ea00dbef123a687bf8e1c197
Yousef Yaser Hajeer <yousefhajeer384@gmail.com>
2026-08-24 14:23:19 +0300

    add active-only name search methods

--- frontend/src/app/core/services/doctor.service.ts
+++ frontend/src/app/core/services/doctor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { DoctorRequest, DoctorResponse } from '../models/doctor.model';
 import { PageResponse } from '../models/department.model';
@@ -42,4 +42,8 @@ export class DoctorService {
   deactivate(id: string): Observable<void> {
     return this.http.patch<void>(`${this.baseUrl}/${id}/deactivate`, {});
   }
+
+  searchByName(name: string): Observable<DoctorResponse[]> {
+  return this.list(name, null, true, 0, 10).pipe(map((res) => res.content));
+}
 }
--- frontend/src/app/core/services/patient.service.ts
+++ frontend/src/app/core/services/patient.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { PatientRequest, PatientResponse } from '../models/patient.model';
 import { PageResponse } from '../models/department.model';
@@ -42,4 +42,8 @@ export class PatientService {
   deactivate(id: string): Observable<void> {
     return this.http.patch<void>(`${this.baseUrl}/${id}/deactivate`, {});
   }
+
+  searchByName(name: string): Observable<PatientResponse[]> {
+  return this.list(name, '', true, 0, 10).pipe(map((res) => res.content));
+}
 }
